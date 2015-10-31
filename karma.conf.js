2efa7c380cc4e2720f5055c5b8175cb63850236d
DimoDimov <dimo.ivanov.dimov@gmail.com>
2015-10-31 17:47:34 +0000

    directive testing

--- karma.conf.js
+++ karma.conf.js
@@ -18,6 +18,8 @@ module.exports = function(config) {
             //load tests
             'test/unit/libs/test-helpers.js',
             'test/unit/**/*.spec.js',
+            //'test/unit/modules/movieList/movieListDir.spec.js',
+
         ],
 
         // list of files to exclude
--- test/unit/libs/test-helpers.js
+++ test/unit/libs/test-helpers.js
@@ -2,22 +2,20 @@ spyOnAngularService = function (service, methodName, deferred) {
 	return spyOn(service, methodName).and.returnValue(deferred.promise);
 };
 
-changeSpyReturn = function (spyObj, result) {
-	return spyObj.and.returnValue({then: function (fn) {
-		fn(result);
-	}});
+changeSpyReturn = function (spyObj, deferred) {
+	return spyObj.and.returnValue(deferred.promise);
 };
 
-spyOnAngularServiceError = function (service, methodName, result) {
-	return spyOn(service, methodName).and.returnValue({then: function (fn, errorFn) {
-		errorFn(result);
-	}});
-};
+// spyOnAngularServiceError = function (service, methodName, result) {
+// 	return spyOn(service, methodName).and.returnValue({then: function (fn, errorFn) {
+// 		errorFn(result);
+// 	}});
+// };
 
-changeSpyReturnErr = function (spyObj, resultErr) {
-	return spyObj.and.returnValue({then: function (fn, errorFn) {
-		errorFn(resultErr);
-	}});
-};
+// changeSpyReturnErr = function (spyObj, resultErr) {
+// 	return spyObj.and.returnValue({then: function (fn, errorFn) {
+// 		errorFn(resultErr);
+// 	}});
+// };
 
 //jasmine.createSpyObj('authService', ['login', 'logout', 'currentUser']);
